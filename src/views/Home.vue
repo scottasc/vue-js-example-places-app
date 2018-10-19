15d7dcdee1da88e4e71dbbf49a8febffccbcd4b1
scottasc <scottschulfer@gmail.com>
2018-10-18 21:31:54 -0500

    connecting to rails backend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div v-for="place in places">
+      {{ place.name }}: {{ place.address }}
+    </div>
+
+    <div>
+        <input v-model="newPlace.name"> Place name<br/>
+        <input v-model="newPlace.address"> Address<br/>
+        <button v-on:click="addPlace()">Add place</button>
+    </div>
+
+
   </div>
 </template>
 
+<style>
+
+body {
+  background-color: lightblue;
+}
+
+body:hover {
+    background-color: lightgreen;
+    transition: background-color 800ms linear;
+}
+
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      places: [],
+      newPlace: {name: "", address: ""}
+    };
+  },
+  created: function() {
+    axios
+    .get("http://localhost:3000/api/places")
+    .then(function(response) {
+      this.places = response.data;
+    }
+    .bind(this));
+  },
+  methods: {
+    addPlace: function() {
+        var params = {
+                      name: this.newPlace.name,
+                      address: this.newPlace.address
+                     };
+
+        axios
+        .post("http://localhost:3000/api/places", params)
+        .then(function(response) {
+            this.places.push(response.data);
+        }.bind(this));
+        this.newPlace = {name: "", address: ""};
   }
-}
+},
+  computed: {}
+};
 </script>
